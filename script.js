a588a2c17a63a80c28df74959f73d2d7b2998017
Liza Elkina <lizapetkova@yandex.ru>
2025-03-16 19:33:13 +0300

    future: switch slides

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  let slides = document.querySelectorAll(".wrapper .slide");
+  let currentSlide = 0;
+  let slideInterval = setInterval(nextSlide, 5000);
 
+  function nextSlide() {
+    slides[currentSlide].className = "slide";
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].className = "slide _showing";
+  }
+});
